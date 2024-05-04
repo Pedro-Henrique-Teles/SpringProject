954c636c5bcf1b7bc88a7eb067f624451407060c
Pedro Henrique Teles <pedrohenriqueteles732@gmail.com>
2024-05-04 01:33:23 -0300

    Comitando atualizações no controller, e indo dormir

--- src/main/java/com/example/springproject/controllers/ProductController.java
+++ src/main/java/com/example/springproject/controllers/ProductController.java
@@ -1,13 +1,43 @@
 package com.example.springproject.controllers;
 
+import com.example.springproject.dtos.ProductRecordDto;
+import com.example.springproject.models.ProductModel;
 import com.example.springproject.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id")UUID id){
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not foud");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    }
 
 }
